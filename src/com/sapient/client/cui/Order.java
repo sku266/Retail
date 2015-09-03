270c03ea799b4e56205ef9a8ca92b6d73de85045
LsCodeMaster <i.liszewski@comcast.net>
2015-09-03 15:27:04 +0530

    Order Skeleton Class

--- src/com/sapient/client/cui/Order.java
+++ src/com/sapient/client/cui/Order.java
@@ -1,5 +1,63 @@
 package com.sapient.client.cui;
 
-public class Order {
+import java.util.Date;
 
+public class Order {
+	private Date date;
+	private String status;
+	private NewCustomer customer;
+	private Payment payment;
+	private OrderDetail orderdetail;
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	public NewCustomer getCustomer() {
+		return customer;
+	}
+	
+	public void setCustomer(NewCustomer customer) {
+		this.customer = customer;
+	}
+	
+	public Payment getPayment() {
+		return payment;
+	}
+	
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+	
+	public OrderDetail getOrderdetail() {
+		return orderdetail;
+	}
+	
+	public void setOrderdetail(OrderDetail orderdetail) {
+		this.orderdetail = orderdetail;
+	}
+	
+	public double calcTax(){
+		return 0;
+	}
+	
+	public double calcTotal(){
+		return 0;
+	}
+	
+	public double calcTotalWeight(){
+		return 0;
+	}
 }
