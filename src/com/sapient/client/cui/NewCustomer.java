6f4fd2a7c05822d96305c5ba153a252334d73e7b
LsCodeMaster <i.liszewski@comcast.net>
2015-09-03 15:59:54 +0530

    newCustomer skeleton code

--- src/com/sapient/client/cui/NewCustomer.java
+++ src/com/sapient/client/cui/NewCustomer.java
@@ -8,5 +8,32 @@ package com.sapient.client.cui;
  *
  */
 public class NewCustomer {
-
+	private String name;
+	private String address;
+	private Order order;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public Order getOrder() {
+		return order;
+	}
+	
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	
 }
