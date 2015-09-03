c886d406990728d2fd695ac1d850873f786e88c0
vinaymattu <vmattu9@gmail.com>
2015-09-03 16:11:26 +0530

    item and order detail skeleton class

--- src/com/sapient/client/cui/Item.java
+++ src/com/sapient/client/cui/Item.java
@@ -3,10 +3,46 @@
  */
 package com.sapient.client.cui;
 
-/**
- * @author sku266
- *
- */
-public class Item {
 
+public class Item {
+ 
+	int shippingWeight;
+	String Description;
+	
+	OrderDetail Orderdetails;
+	
+	public OrderDetail getOrderdetails() {
+		return Orderdetails;
+	}
+	public void setOrderdetails(OrderDetail orderdetails) {
+		Orderdetails = orderdetails;
+	}
+	public int getShippingWeight() {
+		return shippingWeight;
+	}
+	public void setShippingWeight(int shippingWeight) {
+		this.shippingWeight = shippingWeight;
+	}
+	public String getDescription() {
+		return Description;
+	}
+	public void setDescription(String description) {
+		Description = description;
+	}
+	
+
+	
+	public int getPriceforQuantity()
+	{
+		return (Integer) null;
+	}
+	
+	
+	public int getWeight()
+	{
+		return (Integer) null;
+	}
+	
+	
+	
 }
--- src/com/sapient/client/cui/OrderDetail.java
+++ src/com/sapient/client/cui/OrderDetail.java
@@ -3,10 +3,55 @@
  */
 package com.sapient.client.cui;
 
-/**
- * @author sku266
- *
- */
+
 public class OrderDetail {
 
+	int quantity;
+	Boolean taxstatus=true;
+	
+	Order order;
+	
+	Item item;
+	public Item getItem() {
+		return item;
+	}
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public Boolean getTaxstatus() {
+		return taxstatus;
+	}
+	public void setTaxstatus(Boolean taxstatus) {
+		this.taxstatus = taxstatus;
+	}
+	
+	
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	
+	public int CalSubTotal()
+	{
+		return (Integer) null;
+	}
+	
+	
+	public int CalWeight()
+	{
+		return quantity;
+		
+	}
+	
+
+	
+	
 }
